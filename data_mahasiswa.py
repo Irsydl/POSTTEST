96c7e43527f468202f2f97a284d7964616d2c83e
Irsydl <92903763+Irsydl@users.noreply.github.com>
2021-10-24 23:26:01 +0800

    Add files via upload

---
+++ data_mahasiswa.py
@@ -0,0 +1,57 @@
+def data_mahasiswa():
+    # Menerima input data mahasiswa dari pengguna / user
+    nama = input("Nama mahasiswa: ")
+    nim = int(input("NIM: "))
+    prodi = input("Prodi: ")
+    agama = input("Agama: ")
+    tinggi = float(input("Tinggi: "))
+    
+    # Menyimpan hasil input ke dalam list
+    list1 = [nama, nim, prodi, agama, tinggi]
+
+    # Data yang disimpan di list1 dipanggil ke dalam dictionary
+    student_dicti = {
+        'Nama: ': list1[0],
+        'Nim: ': list1[1],
+        'Prodi: ': list1[2],
+        'Agama: ': list1[3],
+        'Tinggi: ': list1[4]
+    }
+    
+    # Output dictionary
+    print(student_dicti)
+
+
+def menu():
+    # Membuat menu tampilan
+    print("[Y] : Masuk ke program ")
+    print("[N] : Keluar dari program ")
+    
+    # Menerima input dari user dan membuat input menjadi huruf kecil
+    user = input("Masukkan Pilihan: ")
+    user = user.lower()
+    
+    # Loop ketika input user tidak sama dengan "n" dan mengeksekusi program data mahasiswa ketika input user sama dengan "y"
+    # Program selesai atau keluar ketika user menginput "n"
+    while user != "n":
+        if user == "y":
+            data_mahasiswa()
+        else:
+            print('"Tidak valid"')
+        
+        print()
+        print("[Y] : Masuk ke program ")
+        print("[N] : Keluar dari program ")
+
+        user = input("Masukkan Pilihan: ")
+        user = user.lower()
+
+menu()
+print('"Program selesai."')
+
+"""
+
+LINK REPO GITHUB:
+https://github.com/Irsydl/POSTTEST2.git
+
+"""
